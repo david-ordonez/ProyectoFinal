598faddd8e005e83e0cb91b73b0f6e2dcf4772e6
David Ordoñez <david.ordonez@live.com.mx>
2022-09-27 01:17:55 -0500

    refactor

--- src/controllers/carritos.js
+++ src/controllers/carritos.js
@@ -5,5 +5,46 @@ export default class ControladorCarritos {
         this.ServiciosCarritos = new ServiciosCarritos();
     }
 
-    
+    getCarritos = async(req,res) => {
+        try {
+            const carritos = await this.ServiciosCarritos.getCarritos();
+            res.send(carritos);
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+            throw error;
+        }
+    };
+
+    getCarrito = async(req,res) => {
+        try {
+            const id = req.params.id;
+            const carrito = await this.Servicioscarritos.getCarritoById(id);
+            res.send(carrito);
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+            throw error;
+        }
+    };
+
+    guardarCarrito = async (req,res) => {
+        try {
+            const carrito = req.body;
+            const carritoGuardado = this.Servicioscarritos.saveCarrito(carrito);
+            res.json(carritoGuardado);
+        } catch (error) {
+            console.log('error al guardar productos', error);
+            res.status(500).json({ error: error.message });
+        }
+    };
+
+    borrarCarrito = async(req,res) => {
+        try {
+            const id = req.params.id;
+            await this.Servicioscarritos.deleteCarrito(id);
+            res.send();
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+            throw error;
+        }
+    };
 }
--- src/routers/api/carritos.js
+++ src/routers/api/carritos.js
@@ -1,27 +1,15 @@
 import { Router } from 'express';
+import ControladorCarritos from '../../controllers/carritos.js';
 
 
 const carritoRouter = new Router();
+const controladorCarritos = new ControladorCarritos();
 
 
-carritoRouter.get('/', async (req, res) => {
-    res.send(await carrito.getAll());
-});
-
-carritoRouter.post('/', async (req, res) => {
-    const newCarrito = req.body;
-    if (newCarrito) return res.send(await carrito.save(newCarrito));
-
-    res.status(400).send({ error: 'Error al agregar' });
-});
-
-carritoRouter.delete('/:id', async (req, res) => {
-    const id = parseInt(req.params.id);
-    const carrito = await carrito.getById(id);
-    if (carrito) return res.send(await carrito.DeleteById(id));
-
-    res.status(404).send({ error: 'carrito no encontrado' });
-});
+carritoRouter.get('/', controladorCarritos.getCarritos);
+carritoRouter.get('/:id', controladorCarritos.getCarrito);
+carritoRouter.post('/', controladorCarritos.guardarCarrito);
+carritoRouter.delete('/:id', controladorCarritos.borrarCarrito);
 
 carritoRouter.get('/:id/productos', async (req, res) => {
     const id = parseInt(req.params.id);
